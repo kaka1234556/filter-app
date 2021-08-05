8d721bd7a0c57f5bfc230d2de5b869f733252fba
kaka1234556 <80517682+kaka1234556@users.noreply.github.com>
2021-08-05 14:52:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+function preload()
+{
+
+}
+
+function setup()
+{
+Canvas=createCanvas(850,450);
+Canvas.center();
+
+}
+
+
+function draw()
+{
+}
+function take_snapshot()
+{
+save('#photo_booth.png');
+}
+
+
+
+
