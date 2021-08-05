2634ef77433d3b1eb05ad08a7f3a99a91c7993e4
kaka1234556 <80517682+kaka1234556@users.noreply.github.com>
2021-08-05 15:16:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,10 +8,13 @@ function setup()
 {
 Canvas=createCanvas(850,450);
 Canvas.center();
-
+video=createCapture(VIDEO);
+video.hide();
+video.size(850,450);
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+    
 }
-
-
 function draw()
 {
     image(video,0,0,850,450);
@@ -20,6 +23,22 @@ function take_snapshot()
 {
 save('#photo_booth.png');
 
+}
+
+function modelLoaded()
+{
+console.log("PoseNet is initialised");
+
+}
+
+function gotPoses(results)
+{
+if(results.length>0)
+{
+console.log(results);
+console.log("mustache x = "+results[0].pose.nose.x);
+console.log("mustache y = "+results[0].pose.nose.y);
+}
 }
 
 
